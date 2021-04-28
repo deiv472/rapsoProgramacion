f16af204dee838b6dc61336e62b85a8184702837
unknown <ramosvilladavid1@gmail.com>
2021-04-28 13:14:21 +0200

    Second Commit

---
+++ repasoIf.js
@@ -0,0 +1,75 @@
+//1.- IF
+function getZodiacSign(d, m) {
+    if ((m == 1 && d <= 20) || (m == 12 && d >= 22)) {
+        return "Capricornio";
+    }
+    else if ((m == 1 && d >= 21) || (m == 2 && d <= 18)) {
+        return "aquarius";
+    }
+    else if ((m == 2 && d >= 19) || (m == 3 && d <= 20)) {
+        return "piscis";
+    }
+    else if ((m == 3 && d >= 21) || (m == 4 && d <= 20)) {
+        return "aries";
+    }
+    else if ((m == 4 && d >= 21) || (m == 5 && d <= 20)) {
+        return "tauro";
+    }
+    else if ((m == 5 && d >= 21) || (m == 6 && d <= 20)) {
+        return "geminis";
+    }
+    else if ((m == 6 && d >= 22) || (m == 7 && d <= 22)) {
+        return "cancer";
+    }
+    else if ((m == 7 && d >= 23) || (m == 8 && d <= 23)) {
+        return "leo";
+    }
+    else if ((m == 8 && d >= 24) || (m == 9 && d <= 23)) {
+        return "virgo";
+    }
+    else if ((m == 9 && d >= 24) || (m == 10 && d <= 23)) {
+        return "libra";
+    }
+    else if ((m == 10 && d >= 24) || (m == 11 && d <= 22)) {
+        return "escorpio";
+    }
+    else if ((m == 11 && d >= 23) || (m == 12 && d <= 21)) {
+        return "sagitario";
+    }
+}
+console.log(getZodiacSign(17, 1));
+/***************************************************************************************** */
+//2.- IF
+function continente(name) {
+    if (name == "España" || "Francia" || "Italia" || "Portugal" || "Alemania") {
+        return ("Europa");
+    }
+    else if (name == "Perú" || "EEUU" || "Ecuador" || "Canadá" || "Mexico") {
+        return ("America");
+    }
+    else if (name == "Australia" || "Nueva Zelanda") {
+        return ("Oceania");
+    }
+    else if (name == "Marruecos" || "Guinea Ecuatorial" || "Argelia" || "Nigeria") {
+        return ("Africa");
+    }
+    else if (name == "China" || "India" || "Korea" || "Indonesia") {
+        return ("Asia");
+    }
+    else {
+        return ("no has introducido ningún pais");
+    }
+}
+console.log(continente("España"));
+/****************************************************************************** */
+//9.- IF
+function esPar(num) {
+    if (num % 2 == 0) {
+        return "El numero es par";
+    }
+    else {
+        return "El numero es impar";
+    }
+}
+console.log(esPar(5));
+/******************************************************************************* */ 
